be82f60b6719ae081538a3226bb2c75d5f47a05c
ReedMcGee <pmcgee1@atu.edu>
2025-01-22 22:50:13 -0600

    Update Practice.cpp

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout << "testing for github" << endl;
+    cout << "testing for github" << endl; // Captitalized letters
     cout << "pulling from github" << endl; //corrected cout statement
 }
 
